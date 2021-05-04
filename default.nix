c61b9d484e2f8be56150544cd9dfc7423be17492
Jesse Gibson <JesseTheGibson@gmail.com>
2021-05-04 11:38:23 -0700

    Convert plugin list to a plugin set
    Conventional usage (and the usage described in the readme) expects
    a plugin set rather than a list. My dotfiles treated it as a list of
    plugins which allowed me to be lazier. I've only recently learned the
    syntax for dynamic keys.

--- default.nix
+++ default.nix
@@ -2,22 +2,26 @@
 
 with pkgs;
 
-let pluginManifest =
-  let lockfile = builtins.fromJSON (builtins.readFile ./lockfile.json);
-  in lockfile.plugins;
+let
+  pluginManifest =
+    let lockfile = builtins.fromJSON (builtins.readFile ./lockfile.json);
+    in lockfile.plugins;
 
-  vimPluginFromDefinition = plugin: vimUtils.buildVimPluginFrom2Nix {
-    pname = builtins.replaceStrings ["."] ["-"] plugin.repo;
-    version = plugin.version;
-    meta.homepage = plugin.homepage;
+    vimPluginFromDefinition = plugin: vimUtils.buildVimPluginFrom2Nix {
+      pname = builtins.replaceStrings ["."] ["-"] plugin.repo;
+      version = plugin.version;
+      meta.homepage = plugin.homepage;
 
-    src = fetchFromGitHub {
-      owner = plugin.owner;
-      repo = plugin.repo;
-      rev = plugin.rev;
-      sha256 = plugin.hash;
-      fetchSubmodules = true;
+      src = fetchFromGitHub {
+        owner = plugin.owner;
+        repo = plugin.repo;
+        rev = plugin.rev;
+        sha256 = plugin.hash;
+        fetchSubmodules = true;
+      };
     };
-  };
 
-in map vimPluginFromDefinition pluginManifest
+  pluginList = map vimPluginFromDefinition pluginManifest;
+
+# Map plugins to a set of: { plugin-name => plugin }
+in builtins.foldl' (pluginSet: plugin: pluginSet // { ${plugin.pname} = plugin; }) {} pluginList
