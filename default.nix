940fba6d63964db903d9899ec8eaa603f85d516a
Jesse Gibson <JesseTheGibson@gmail.com>
2021-05-04 10:46:45 -0700

    Import lockfile generator from personal dotfiles
    The code in this repository is mostly inspired by a trick I used in my
    dotfiles framework. After switching to NixOS, a lot of my dotfiles
    framework became irrelevant. I need to rewrite it, which is part of what
    this repository is for.
    
    Anyway, I've copied over several files and modified them to work
    standalone. Now I can use plugins from this repo as a custom nix
    channel.

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let pluginManifest =
+  let lockfile = builtins.fromJSON (builtins.readFile ./lockfile.json);
+  in lockfile.plugins;
+
+  vimPluginFromDefinition = plugin: vimUtils.buildVimPluginFrom2Nix {
+    pname = builtins.replaceStrings ["."] ["-"] plugin.repo;
+    version = plugin.version;
+    meta.homepage = plugin.homepage;
+
+    src = fetchFromGitHub {
+      owner = plugin.owner;
+      repo = plugin.repo;
+      rev = plugin.rev;
+      sha256 = plugin.hash;
+      fetchSubmodules = true;
+    };
+  };
+
+in map vimPluginFromDefinition pluginManifest
---
+++ plugin-names.js
@@ -0,0 +1,16 @@
+module.exports = [
+  // TODO: Put these in nixpkgs.
+  'PsychoLlama/further.vim',
+  'PsychoLlama/teleport.vim',
+  'PsychoLlama/alternaut.vim',
+  'sevko/vim-nand2tetris-syntax',
+  'othree/yajs.vim',
+  'chr4/nginx.vim',
+  'mxw/vim-jsx',
+
+  // TODO: Find a better markdown previewer.
+  'davinche/godown-vim',
+
+  // TODO: Make this plugin less shameful.
+  'PsychoLlama/navitron.vim',
+];
