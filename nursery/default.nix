eb95b2c418f2a77deb626612de837f01d80e2344
Jesse Gibson <JesseTheGibson@gmail.com>
2021-05-04 23:12:00 -0700

    Remove nursery plugin name prefix
    It's no longer necessary. This was an artifact of the old dotfiles
    implementation where packages were installed in a list, not a set, and
    didn't need to be mentioned by name. Specificity was free.

--- nursery/default.nix
+++ nursery/default.nix
@@ -9,7 +9,7 @@ let
     in map intoPlugin pluginNames;
 
   wrapPlugin = { name, path }: pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "nursery-${builtins.replaceStrings ["."] ["-"] name}";
+    pname = builtins.replaceStrings ["."] ["-"] name;
     version = "latest";
     src = path;
   };
