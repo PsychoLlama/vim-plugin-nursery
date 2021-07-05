6610f88bc0eb24a94aa863b31fccf1e3fb4b778b
Jesse Gibson <JesseTheGibson@gmail.com>
2021-07-04 20:02:22 -0700

    Package 3rd-party plugins in a Nix flake
    I think Nix flakes are perfect to replace my manual version pinning
    script. It functions nearly identical, except Nix is responsible for
    tracking revisions in the lockfile, and this way I can distribute in
    a more standardized format.

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "A sandbox for developing experimental vim plugins";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+    # TODO: Put these in nixpkgs.
+    further-vim = {
+      url = "github:PsychoLlama/further.vim/master";
+      flake = false;
+    };
+
+    teleport-vim = {
+      url = "github:PsychoLlama/teleport.vim/master";
+      flake = false;
+    };
+
+    alternaut-vim = {
+      url = "github:PsychoLlama/alternaut.vim/main";
+      flake = false;
+    };
+
+    vim-nand2tetris = {
+      url = "github:sevko/vim-nand2tetris-syntax/master";
+      flake = false;
+    };
+
+    yajs-vim = {
+      url = "github:othree/yajs.vim";
+      flake = false;
+    };
+
+    nginx-vim = {
+      url = "github:chr4/nginx.vim";
+      flake = false;
+    };
+
+    vim-jsx = {
+      url = "github:mxw/vim-jsx";
+      flake = false;
+    };
+
+    # TODO: Find a better markdown previewer.
+    godown-vim = {
+      url = "github:davinche/godown-vim";
+      flake = false;
+    };
+
+    # TODO: Make this plugin less shameful.
+    navitron-vim = {
+      url = "github:PsychoLlama/navitron.vim";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self, nixpkgs, further-vim, teleport-vim, alternaut-vim,
+    vim-nand2tetris, yajs-vim, nginx-vim, vim-jsx, godown-vim,
+    navitron-vim,
+  }:
+
+  let
+    buildPlugin = system: plugin:
+      let inherit (nixpkgs.legacyPackages.${system}) vimUtils;
+      in vimUtils.buildVimPluginFrom2Nix plugin;
+
+    buildAllPlugins = system: _: builtins.mapAttrs
+      (pluginName: plugin: buildPlugin system {
+        pname = pluginName;
+        version = plugin.rev;
+        src = plugin;
+      })
+      {
+        inherit further-vim teleport-vim alternaut-vim vim-nand2tetris;
+        inherit yajs-vim nginx-vim vim-jsx godown-vim navitron-vim;
+      };
+
+  in {
+    packages = builtins.mapAttrs buildAllPlugins {
+      "x86_64-linux" = null;
+      "i686-linux" = null;
+      "x86_64-darwin" = null;
+      "aarch64-linux" = null;
+      "armv6l-linux" = null;
+      "armv7l-linux" = null;
+      "aarch64-darwin" = null;
+    };
+  };
+}
